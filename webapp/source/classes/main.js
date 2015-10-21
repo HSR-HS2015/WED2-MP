d92e71651ac638991035d587d9066540432e78c2
Fabian Binna <fbinna@hsr.ch>
2015-10-21 13:49:08 +0200

    requirejs dependencies

---
+++ webapp/source/classes/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by fabian on 21/10/2015.
+ */
+require.config({
+    // base url relative to the index.html
+    baseUrl: './',
+    paths: {
+        'angular': 'frameworks/angular/angular.min'
+    },
+    // angular does not support async loading out of the box -> use the shim loader
+    shim: {
+        'angular': {
+            exports: 'angular'
+        }
+    }
+});
+
+define(['angular'], function (angular) {
+    var app = angular.module("lafete");
+    return angular.bootstrap(app);
+});
